4307590bfea5678214e83e1e0eac5e83d0cff9f7
neerajt <neerajtandon25@gmail.com>
2015-03-07 16:20:06 -0500

    modifying campuses.js

--- campuses.js
+++ campuses.js
@@ -1,30 +1,11 @@
-function getCampusesByDistrictName(){
-	    var campusesByDistrictId = {};
-			    var districtsById = {};
-					    var campusesByDistrictName = {};
+function getCampusids(){
+	var campuses = {};
 							 
-							    for(var i = 0; i < camp.length; i ++){
-										        campusesByDistrictId[camp[i][1]] = campusesByDistrictId[camp[i][1]] || [];
-														        campusesByDistrictId[camp[i][1]].push({
-																			            campusId : camp[i][0],
-																			            districtId : camp[i][1],
-																			            campusName : camp[i][2],
-																			            districtName : camp[i][3],
-																			            regionId : camp[i][4],
-																			            countyName : camp[i][5]
-																			        });
-																		    }
-									 
-									    for(var i = 0; i < dist.length; i ++){
-												        districtsById[dist[i][0]] = dist[i][1];
-																    }
-											 
-											    for (var districtId in campusesByDistrictId) {
-														       var campusArray = campusesByDistrictId[districtId];
-																	        campusesByDistrictName[districtsById[districtId]] = campusArray;
-																					    }
-													 
-													    return campusesByDistrictName;
+	for(var i = 0; i < camp.length; i ++){
+		campuses[i] : camp[i][0],
+		});
+	
+	return campuses;
 }
  
-var campusesByDistrictName = getCampusesByDistrictName();
+var campuses = getCampusids();
