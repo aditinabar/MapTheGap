01a179dd9195a8b2150d273a33a3082e01841b36
neerajt <neerajtandon25@gmail.com>
2015-03-18 20:31:13 -0400

    cleaning up

--- campuses.js
+++
@@ -1,9 +0,0 @@
-function getCampusids(){
-	var campuses = [];
-	for(var i = 0; i < camp.length; i ++){
-		campuses.push(camp[i][0]);
-	};
-	return campuses;
-}
- 
-var campuses = getCampusids();
--- getPDFs.sh
+++
@@ -1,4 +0,0 @@
-while read -r -a myArray
-do 
-	wget http://ritter.tea.state.tx.us/perfreport/src/2014/static/campus/c${myArray[0]}.pdf
-done < CampusID.txt
