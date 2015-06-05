3d919f30ea6bd3cc7c550ed99d68fa2922bcefdd
aditinabar <ahnabar@gmail.com>
2015-06-05 12:33:40 -0500

    Found way to identify zipcodes in com that are in tx

--- Community/Code/ZipInTX.R
+++ Community/Code/ZipInTX.R
@@ -1,12 +1,12 @@
 setwd("C:/Users/naditi/Projects/MapTheGap/Community/Data/")
-zip <- read.table("US Zipcodes.csv", sep = ",", header = TRUE, stringsAsFactor = TRUE)
+US <- read.table("US Zipcodes.csv", sep = ",", header = TRUE, stringsAsFactor = TRUE)
 
-zip[,which(colnames(zip) == "world_region")] <- NULL
-zip[,which(colnames(zip) == "timezone")] <- NULL
-zip[,which(colnames(zip) == "county")] <- NULL
-zip[,which(colnames(zip) == "area_codes")] <- NULL
+US[,which(colnames(US) == "world_region")] <- NULL
+US[,which(colnames(US) == "timezone")] <- NULL
+US[,which(colnames(US) == "county")] <- NULL
+US[,which(colnames(US) == "area_codes")] <- NULL
 
-zip <- zip[zip$type=="STANDARD",]
+zip <- US[US$type=="STANDARD",]
 row.names(zip) <- NULL
 
 tx <- zip[zip$state == "TX",]
@@ -16,43 +16,37 @@ row.names(tx) <- NULL
 setwd("C:/Users/naditi/Projects/MapTheGap/Community/Data/Clean")
 com <- read.table("Community.csv", header = TRUE, sep = ",", stringsAsFactors = TRUE)
 
-View(zip)
-View(com)
+## Winning Method 
 
-ziptx <- data.frame(tx$zip)
-as.vector(unlist(ziptx), mode = "numeric")
+texas2 <- com[com$zip %in% tx$zip,]
 
-zipcom <- data.frame(com$Zip)
-as.vector(unlist(zipcom), mode = "numeric")
-
-Texas <- vector()
-ext <- vector()
-
-## Method A
-for (i in 1:nrow(zipcom)){
-  if(((as.numeric(zipcom[i,]) %in% unlist(ziptx))) == TRUE){
-    Texas[i] <- as.numeric(zipcom[i,])
-  }
-  else if(((as.numeric(zipcom[i,]) %in% unlist(ziptx))) == FALSE){
-    ext[i] <- as.numeric(zipcom[i,])
-  }
-}
-
-## Method B
-texas2 <- zipcom[match(zipcom, ziptx),]
-
-## Method C
-texas3 <- subset(zipcom, zipcom %in% ziptx)
-row.names(texas3) <- NULL
-
-
-
-
-# # Testing
-# v <- c("a", "b", "c", "d")
 # 
-# "d" %in% v
+# ziptx <- as.vector(tx$zip)
+# #as.vector(unlist(ziptx), mode = "numeric")
+# 
+# zipcom <- as.vector(com$Zip)
+# #as.vector(unlist(zipcom), mode = "numeric")
+# 
+# Texas <- vector()
+# ext <- vector()
 
+# ## Method A
+# for (i in 1:1849){
+#   if((zipcom[i] %in% ziptx) == "TRUE"){
+#     print(zipcom[i])
+#     Texas[i] <- zipcom[i]
+#   }
+#   else if(((as.numeric(zipcom[i]) %in% unlist(ziptx))) == "FALSE"){
+#     ext[i] <- as.numeric(zipcom[i])
+#   }
+# }
+
+
+
+# Testing
+v <- c("a", "b", "c", "d")
+
+"d" %in% v
 
 
 
